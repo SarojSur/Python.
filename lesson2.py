4dcd7a82d27f3fa958d8a3e15316c3f3a7287fca
Sarojsur <rathindra101@gmail.com>
2025-04-18 22:55:09 +0600

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,34 @@
+print("Welcome to Python")
+
+w=60
+p=12
+print(w+p)
+
+name ="saroj"  #String
+age = 1000#integer
+Goodboy =True#Boolean
+mathmark =98.5 #float
+
+print("My name is ", name ,"I am ",age,"years old")
+ 
+ #Operator
+a = 9
+g = 5
+
+ #Arithmatic Operators
+print(a+g)
+print(a-g)
+print(a*g)
+print(a/g)
+
+ #Logical operators
+print(a>g)
+print(a<g)
+print(a==g)
+print(a!=g)
+
+k=500
+if (a<9.9) :
+    print("K is greater than 9.9")
+else:
+    print("9.9 is less than k")
