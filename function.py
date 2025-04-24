e7c627a8a7a0e44d8d4be32b4acd99388f0f5f48
Sarojsur <rathindra101@gmail.com>
2025-04-24 21:55:04 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,56 @@
+def wish():
+    print("happy birthday pala dynasty")
+
+def GM():
+    print("Good morning!!!")
+
+def gd():
+    print("Good night")
+
+GM()
+wish()
+gd()
+wish()
+gd()
+
+
+def wish(name):
+    print("happy birthday pala",name)
+
+wish("Pala Dynasty")
+
+
+def wish(number):
+    print("I am  ",number)
+
+wish("9+3")
+
+
+def Add(a,b):
+    print(a*b)
+
+Add(9000,0.25)
+
+
+def Add(a,b):
+    print(a/b)
+
+Add(8000,0.25)
+
+
+def Add(a,b):
+    print(a+b)
+
+Add(7000,0.25)
+
+
+def Add(a,b):
+    print(a-b)
+
+Add(6000,0.25)
+
+#Input
+
+name = input("Enter your Name: ")
+print(name)
+
