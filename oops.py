418c969c668690f1407ef02ae2d76a1e87cf5fc0
Sarojsur <rathindra101@gmail.com>
2025-04-25 22:50:43 +0600

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,42 @@
+#class
+class human:
+    hand = 2
+    leg = 2
+    kidny = 1
+    leg = 2
+    cell = 10000
+
+
+saroj = human()
+
+print(saroj.hand)
+
+class plants:
+    steam = 1
+    leaves = 212151214
+    root = 1
+    cell = 14112147
+
+Rose = plants()
+print(plants.root)
+
+
+class car:
+    whell = 4
+    glass = 1
+    seat = 5
+    
+
+nothing = car()
+print(car.whell)
+
+
+class students:
+    def hobby(self):
+        print("reading books")
+        print("playing in the in field")
+        print("Singing")
+        print("dancing")
+
+arka = students()
+arka.hobby()
