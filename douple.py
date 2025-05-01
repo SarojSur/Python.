f53b8cd574b18dca7d1171bdb19faed55306e6db
Sarojsur <rathindra101@gmail.com>
2025-05-01 21:51:33 +0600

    Add files via upload

---
+++ douple.py
@@ -0,0 +1,53 @@
+fruits = ("Mango","Guava","Apple","Grape","Pineapple","lichi")
+
+print(fruits)
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+print(fruits[4])
+print(fruits[5])
+
+
+spices = ("Ginger","Chilli","Garlic","Onion","Red Chilli","lichi")
+
+print(spices)
+
+print(spices[0])
+print(spices[1])
+print(spices[2])
+print(spices[3])
+print(spices[4])
+print(spices[5])
+
+player = ("Messi","Neymar","Raphinia","Vini JR","Rodrigo","Ronaldo")
+
+print(player)
+print(player[0])
+print(player[1])
+print(player[2])
+print(player[3])
+print(player[4])
+print(player[5])
+
+a = (1,2)
+b = (3,4)
+
+print(a+b)
+print(a*2)
+print(len(a))
+
+a = (80,20)
+b = (30,40)
+
+print(a+b)
+print(a*80)
+print(len(b))
+
+a = (20,70)
+b = (2,79)
+
+print(a+b)
+print(a*20)
+print(len(a+b))
