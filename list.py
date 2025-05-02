2446f76a68604be4ca4b2d940cdc73b3ecf54e3e
Sarojsur <rathindra101@gmail.com>
2025-05-02 22:52:43 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,48 @@
+#List - Collection of Data
+# Tupple - Collection of data
+
+numbers = [1,2,3,4,5,6]
+print(numbers)
+
+
+
+
+#List - Collection of Data
+# Tupple - Collection of data
+
+numbers = [4,90,60,74,36]
+print(numbers)
+
+#List - Collection of Data
+# Tupple - Collection of data
+
+numbers = [2000,500,600,800,700]
+print(numbers)
+
+
+#Element
+
+numbers = [1,2,3,6,9,50]
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+
+
+friends = ["a","p","o","l","u","w","q"]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[5])
+
+
+vegetable = ["carrot","bean"]
+print(vegetable[0])
+print(vegetable[1])
+
+
+#How to add Elements
