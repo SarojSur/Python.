107aa571300314b8677318f955f0d595fcbe50e7
Sarojsur <rathindra101@gmail.com>
2025-05-09 22:50:52 +0600

    Add files via upload

---
+++ fruitbasket.py
@@ -0,0 +1,22 @@
+Fruitbasket = {
+  "Apple":"Red",
+  "Banana": "Yellow",
+  "Jackfruit":"Brown",
+  "Watermelon":"Green",
+   "Strawberry":"Red+Green",
+   "Blackberry":"Black",
+   "Pineapple":"White",
+   "Lichi":"Red+Green+White",
+}
+
+print(Fruitbasket)
+print(Fruitbasket.keys())
+print(Fruitbasket.values())
+print(Fruitbasket.get("Apple"))
+print(Fruitbasket.get("Banana"))
+print(Fruitbasket.get("Jackfruit"))
+print(Fruitbasket.get("Watermelon"))
+print(Fruitbasket.get("Strawberry"))
+print(Fruitbasket.get("BlacKberry"))
+print(Fruitbasket.get("Pineapple"))
+print(Fruitbasket.get("Lichi"))
