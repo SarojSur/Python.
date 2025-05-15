e5cc4a146b0898b7383a7f6e3723670bce134ad5
Sarojsur <rathindra101@gmail.com>
2025-05-15 21:32:03 +0600

    Add files via upload

---
+++ file.py
@@ -0,0 +1,15 @@
+with open("Story.tsk","w") as file:
+    file.write("Once Upon A Time There was a King...")
+
+with open("Story.tsk","w") as file:
+    file.write("This is Hasan Jamil...")
+
+with open("Story.tsk","w") as file:
+    file.write("Next line story...")
+
+
+with open("Story.tsk","w") as file:
+    story = file.read()
+    print(story)with open("Story.tsk","w") as file:
+    story = file.read()
+    print(story)
