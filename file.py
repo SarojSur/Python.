fed2a32a3aa8880b82b0c68d125d431fc014f365
Sarojsur <rathindra101@gmail.com>
2025-05-16 23:06:37 +0600

    Add files via upload

--- file.py
+++ file.py
@@ -1,15 +1,51 @@
-with open("Story.tsk","w") as file:
-    file.write("Once Upon A Time There was a King...")
+with open("Story.tsk","w") as file :
+    file.write("Once a man can became Lion...")
 
-with open("Story.tsk","w") as file:
+with open("Story.tsk","a") as file :
     file.write("This is Hasan Jamil...")
 
-with open("Story.tsk","w") as file:
-    file.write("Next line story...")
+with open("Story.tsk","a") as file :
+    file.write("Next line story is very dengerious for human...")
 
 
-with open("Story.tsk","w") as file:
-    story = file.read()
-    print(story)with open("Story.tsk","w") as file:
-    story = file.read()
-    print(story)
+file = open("Story.tsk","r")
+print(file.readlines())
+
+with open("Story.tsk","w") as file :
+    file.write("Once a man can became Lion...")
+
+with open("Story.tsk","a") as file :
+    file.write("This is Hasan Jamil...")
+
+with open("Story.tsk","a") as file :
+    file.write("Next line story is very dengerious for human...")
+
+
+file = open("Story.tsk","r")
+print(file.readlines())
+
+with open("Story.tsk","w") as file :
+    file.write("Once a man can became Lion...")
+
+with open("Story.tsk","a") as file :
+    file.write("This is Hasan Jamil...")
+
+with open("Story.tsk","a") as file :
+    file.write("Next line story is very dengerious for human...")
+
+
+file = open("Story.tsk","r")
+print(file.readlines())
+
+with open("Story.tsk","w") as file :
+    file.write("Once a man can became Lion...")
+
+with open("Story.tsk","a") as file :
+    file.write("This is Hasan Jamil...")
+
+with open("Story.tsk","a") as file :
+    file.write("Next line story is very dengerious for human...")
+
+
+file = open("Story.tsk","r")
+print(file.readlines())
